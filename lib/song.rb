2d453d6efc9a30ce7c22070e9f452ed79ad8ead4
taroj khan <tarujkhan@yahoo.com>
2020-03-14 00:56:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,19 +9,19 @@ class Song
   def save
     self.class.all << self
   end
- def self.create #class constructor
+ def self.create
     song = self.new
     self.all << song
     song
   end
 
-  def self.new_by_name(title) #class constructor
+  def self.new_by_name(title)
     song = self.new
     song.name = title
     song
   end
 
-  def self.create_by_name(title) #class constructor
+  def self.create_by_name(title) 
     song = self.create
     song.name = title
     song
